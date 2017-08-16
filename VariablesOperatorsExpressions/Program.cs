d67072599c58bb3276583749369ea523113da56c
karenfarthing <karenf_email@yahoo.com>
2017-08-16 18:36:04 -0400

    worked on practice problems

--- VariablesOperatorsExpressions/Program.cs
+++ VariablesOperatorsExpressions/Program.cs
@@ -10,6 +10,7 @@ namespace VariablesOperatorsExpressions
     {
         static void Main(string[] args)
         {
+            /* comment these lines off
             //strings - use double quotes
 
             string name = "Karen";
@@ -44,7 +45,10 @@ namespace VariablesOperatorsExpressions
             //Character or char uses single quotes - denotes a single character value
             char lastLetter = 'f';
 
-            //Operators
+   end comment section */
+
+            
+    //Operators
 
             //example 1 - translating english to code
             //Jessica is 23 years old.
@@ -53,18 +57,70 @@ namespace VariablesOperatorsExpressions
 
             int jessicaAge = 23;
             int samAge = 47;
+            Console.WriteLine("Is Jessica younger than Sam?");
             Console.WriteLine(jessicaAge < samAge);//returns a boolean value of true or false
 
             //example 2 - mike and 3 friends want to decide bill evenly, each person paid $13 what was total bill?
             int billShare = 13;
             int numFriends = 4;
+            Console.WriteLine("The total bill was (two different ways)");
+
             Console.WriteLine(billShare * numFriends);
             int totalBill = billShare * numFriends; //another way to do this same thing
             Console.WriteLine(totalBill);
-            
+
 
             //example 3 - lisa is cooking muffins. recipe needs 7 c. sugar. She already put in 2 c. how many left?
-            
+            int sugarTotal = 7;
+            int sugarIn = 2;
+            int sugarLeft = sugarTotal - sugarIn;
+            Console.WriteLine("The amount of sugar remaining is " + sugarLeft);
+
+            //example 4 - how many packages of diapers can you buy with $40 if each is $8
+            float costDiapers = 8.00f;
+            float totalMoney = 40.00f;
+            float numDiapers = totalMoney / costDiapers;
+            Console.WriteLine("You can buy " + numDiapers + " packages of diapers");
+
+            //example 5 - last friday Trevon had $29. over the weekend he cleaned the attic. He now has $41. How much did he receive?
+            float startMoney = 29.00f;
+            float nowMoney = 41.00f;
+            float moneyMade = nowMoney - startMoney;
+            Console.WriteLine("Trevor made $" + moneyMade);
+
+            //example 6 - Julia ran 30 miles more than pranav. Julia ran 47 miles. How many miles did Pranav run?
+
+            double juliaRun = 47d;
+            double juliaRunMoreThanPranav = 30d;
+            double pranavRun = juliaRun - juliaRunMoreThanPranav;
+            Console.WriteLine("Pranav ran " + pranavRun + " miles");
+
+            //example 7 mow many boxes of envelopes can you buy with $12 if one box costs $3
+            float envelopeCost = 3.00f;
+            float totalMoney7 = 12.00f;
+            float canBuy = totalMoney7 / envelopeCost;
+            Console.WriteLine("You can buy " + canBuy + " boxes of envelopes");
+
+            //example 8 after paying $5.12 for a salad Norachai has $57.10. How much did he have to start?
+            float saladCost = 5.12f;
+            float norachaiFinalAmt = 57.10f;
+            float norachaiStartAmt = norachaiFinalAmt + saladCost;
+            Console.WriteLine("start amount was $" + norachaiStartAmt);
+
+            //example page 2-1 331 students went on a field trip there were 6 buses and 7 students left in cars. how many people per bus?
+            int numStudents = 331;
+            int numBuses = 6;
+            int studentsInCars = 7;
+            int studentsPerBus = (numStudents - studentsInCars) / numBuses;
+            Console.WriteLine("The number of students in each bus is " + studentsPerBus);
+
+            //example 2-2 aliyah spent
+
+
+
+
+
+
 
 
 
